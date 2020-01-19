4099c6275144c333dcc6a5dec62c28ad5508668f
Maxime Vanderwegen <maximevanderwegen@disroot.org>
2020-01-19 22:18:00 +0100

    Chat improvements

--- src/js/chat/chat.js
+++ src/js/chat/chat.js
@@ -1,7 +1,47 @@
 //Websocket connection
 const socket = io();
 
-$('.msg__container__sendBtn').click(() => {
+// Get the background image from the settings and add it to the page
+$.ajax({
+  url: './settings/settings.json',
+  method: 'GET',
+  dataType: 'json'
+}).done(settings => {
+  if (settings.backgroundImage !== undefined) {
+    const handle404ImageError = () => {
+      $('.msgContainer').text('Sorry, your background image couldn\'t be loaded... Maybe try another one')
+      fade('.msgContainer');
+
+      $('.backgroundImage').css('backgroundImage', '');
+    }
+
+    $.ajax({
+        url: settings.backgroundImage,
+        method: 'GET',
+        dataType: '',
+        statusCode: {
+          200: () => {
+            $('.backgroundImage').css('backgroundImage', `url(${settings.backgroundImage})`);
+          },
+          404: () => {
+            handle404ImageError();
+          }
+        }
+      })
+      .fail(err => {
+        // CORS error handling
+        if (err.statusText === 'error' && err.readyState === 0) {
+          handle404ImageError();
+        }
+      });
+  }
+
+  if (settings.owmToken !== undefined) {
+    owmToken = settings.owmToken;
+  }
+});
+
+$('.msgContainer__form__sendBtn').click(() => {
   let msg = new Message();
   msg.send();
 });
--- src/js/chat/message.js
+++ src/js/chat/message.js
@@ -1,8 +1,13 @@
 const Message = function() {
   this.author = 'Max';
-  this.content = $('.msg__container__inputMsg').val();
+  this.content = $('.msgContainer__form__inputMsg').val();
   this.dateTime = new Date();
   this.send = function() {
+    let msgContainer = $('<span></span>')
+      .addClass('msg flex')
+      .text(`${this.content}`)
+      .appendTo('.msgContainer__list');
+
     socket.emit('chat msg', this.content);
   }
 }
