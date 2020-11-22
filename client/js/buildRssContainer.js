bfee76726ee7fc3d31575cb055002ee3f51f0343
belgianGeek <maximevanderwegen@disroot.org>
2020-11-22 18:43:17 +0100

    Revert "Add pagination for RSS feeds :construction:"
    This reverts commit dd40a2573043e1f12e7c7265431f814301f77d84.

--- client/js/buildRssContainer.js
+++ client/js/buildRssContainer.js
@@ -1,8 +1,15 @@
 const buildRssContainer = (feed, callback) => {
   let rssContainer = $('<section></section>');
   let linksContainer = $('<div></div>');
-  for (let i = 0; i <= 50; i++) {
+  for (let i = 0; i < 10; i++) {
     if (feed[i] !== undefined && feed[i].title !== undefined && feed[i].link !== undefined) {
+      rssContainer
+        .addClass('rssContainer flex');
+
+      linksContainer
+        .addClass('linksContainer')
+        .appendTo(rssContainer);
+
       let article = $('<div></div>')
         .addClass('article flex')
         .appendTo(linksContainer);
@@ -18,21 +25,7 @@ const buildRssContainer = (feed, callback) => {
         .addClass('article__desc')
         .append(feed[i].summary)
         .appendTo(linksContainer);
-
-      if (i % 10 === 0 && i !== 0) {
-        linksContainer
-          .addClass('linksContainer')
-          .appendTo(rssContainer);
-
-        if (i > 10) rssContainer.addClass('rssContainer hidden')
-        else rssContainer.addClass('rssContainer flex');
-
-        callback(feed, rssContainer);
-        console.log(i, feed[i].title);
-
-        rssContainer = $('<section></section>');
-        linksContainer = $('<div></div>');
-      }
     }
-  }
+  };
+  callback(feed, rssContainer);
 }
