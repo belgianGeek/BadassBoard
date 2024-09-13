924fc4bd19b3b8fa501a34e89fb87b890da75beb
Maxime Vanderwegen <39968047+belgianGeek@users.noreply.github.com>
2024-09-13 22:51:08 +0200

    Add components refresh button, style improvements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@ const getInvidiousInstanceHealth = async () => {
         // Remove the final / if any
         globalStore.addInvidiousInstance(res.data[i][1].uri.replace(/\/$/, ''));
       }
-      
+
       if (i === res.data.length - 1) {
         if (globalStore.invidiousInstances[0] === undefined) {
           return 'Unable to retrieve Invidious instances health : instances health is unknown.';
@@ -74,7 +74,7 @@ const goToPreviousPage = async (containerId) => {
   contents.value[containerId].containerPageNumber--;
 };
 
-const modifyContent = async (content, index) => {
+const modifyContent = async (content) => {
   if (content.isModified) {
     content.isModified = false;
   } else {
@@ -117,11 +117,30 @@ onMounted(() => {
     <div class="contentContainers flexRow">
       <section :class="content.type + 'Container'" class="content flexColumn"
         v-for="[iContent, content] of contents.entries()">
-        <button @click="modifyContent(content)" :class="{
+        <span class="content_BtnContainer flexRow">
+          <button @click="modifyContent(content)" :class="{
       hidden: content.isModified,
     }">
-          Modify
-        </button>
+            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <g stroke="#fff" stroke-width="1.5">
+                <path
+                  d="m21.3175 7.14139-.4936-.8566c-.3733-.64783-.5599-.97174-.8775-1.10091-.3176-.12916-.6768-.02724-1.3951.1766l-1.2202.3437c-.4586.10576-.9398.04576-1.3585-.16939l-.3369-.19437c-.3591-.22999-.6353-.56909-.7882-.96768l-.3339-.99738c-.2196-.66002-.3294-.99003-.5908-1.17879-.2613-.18876-.6085-.18876-1.3029-.18876h-1.1148c-.6943 0-1.0415 0-1.3029.18876-.26135.18876-.37114.51877-.59071 1.17879l-.33396.99738c-.15288.39859-.42908.73769-.78816.96768l-.33688.19437c-.41875.21515-.8999.27515-1.35851.16939l-1.22023-.3437c-.71834-.20384-1.0775-.30576-1.39508-.1766-.31758.12917-.50422.45308-.87752 1.10091l-.49358.8566c-.34991.60725-.52487.91088-.49091 1.2341.03395.32322.26817.58369.7366 1.10463l1.03104 1.15268c.252.319.43091.875.43091 1.3749 0 .5001-.17885 1.0559-.43088 1.375l-1.03107 1.1527c-.46843.521-.70264.7814-.7366 1.1047-.03396.3232.141.6268.49091 1.234l.49357.8566c.37329.6478.55995.9718.87753 1.1009.31758.1292.67675.0273 1.3951-.1766l1.22017-.3437c.45869-.1058.93993-.0457 1.35873.1695l.33683.1944c.35901.23.63514.569.788.9676l.33399.9975c.21957.66.32936.99.59071 1.1788.2614.1887.6086.1887 1.3029.1887h1.1148c.6944 0 1.0416 0 1.3029-.1887.2614-.1888.3712-.5188.5908-1.1788l.334-.9975c.1528-.3986.4289-.7376.788-.9676l.3368-.1944c.4188-.2152.9-.2753 1.3587-.1695l1.2202.3437c.7183.2039 1.0775.3058 1.3951.1766.3176-.1291.5042-.4531.8775-1.1009l.4936-.8566c.3499-.6072.5248-.9108.4909-1.234-.034-.3233-.2682-.5837-.7366-1.1047l-1.0311-1.1527c-.252-.3191-.4309-.8749-.4309-1.375 0-.4999.179-1.0559.4309-1.3749l1.0311-1.15268c.4684-.52094.7026-.78141.7366-1.10463.0339-.32322-.141-.62685-.4909-1.2341z"
+                  stroke-linecap="round" />
+                <path
+                  d="m15.5195 12c0 1.933-1.567 3.5-3.5 3.5s-3.49997-1.567-3.49997-3.5 1.56697-3.5 3.49997-3.5 3.5 1.567 3.5 3.5z" />
+              </g>
+            </svg>
+          </button>
+          <button @click="getContent(content.index)" v-if="content.type === 'rss' || content.type === 'weather'">
+            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <g stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                <path
+                  d="m17.4776 9.01106c.0074-.00004.0149-.00005.0224-.00005 2.4853 0 4.5 2.01839 4.5 4.50829 0 2.3205-1.75 4.2315-4 4.4807m-.5224-8.98894c.0148-.165.0224-.3321.0224-.50097 0-3.04314-2.4624-5.51009-5.5-5.51009-2.87676 0-5.23767 2.21267-5.47958 5.03192m10.95718.97914c-.1023 1.13654-.549 2.17354-1.2348 3.00544m-9.72238-3.98458c-2.53644.24181-4.52042 2.38198-4.52042 4.98638 0 2.4234 1.71776 4.4449 4 4.909m.52042-9.89538c.15784-.01505.31781-.02275.47958-.02275 1.12582 0 2.16474.37277 3.0005 1.00184" />
+                <path d="m12 21v-8m0 8c-.7002 0-2.00847-1.9943-2.5-2.5m2.5 2.5c.7002 0 2.0085-1.9943 2.5-2.5" />
+              </g>
+            </svg>
+          </button>
+        </span>
         <nav class="contentNav" :class="{
       hidden: !content.isModified,
       flexColumn: content.isModified,
@@ -225,6 +244,17 @@ onMounted(() => {
   margin: 1em;
   padding: 1em;
   max-height: 45vh;
+
+  &_BtnContainer {
+    align-self: flex-end;
+
+    button {
+      background: none;
+      border: none;
+      margin: 0 .3em;
+      cursor: pointer;
+    }
+  }
 
   &Nav {
     &__label {
